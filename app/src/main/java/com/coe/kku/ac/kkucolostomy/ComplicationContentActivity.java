71ea75f594623c8db04d3f3b5778c6051830afb4
chompusama <chadaporn.p@kkumai.com>
2019-10-20 06:26:38 +0700

    add everything related to Complication

---
+++ app/src/main/java/com/coe/kku/ac/kkucolostomy/ComplicationContentActivity.java
@@ -0,0 +1,97 @@
+package com.coe.kku.ac.kkucolostomy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ComplicationContentActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView title, subtitle, care, careTitle;
+    private ImageView img;
+    private ImageButton backBtn;
+    private int content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complication_content);
+
+        backBtn = (ImageButton) findViewById(R.id.compli_content_back_btn);
+
+        title = (TextView) findViewById(R.id.compli_content_title);
+        subtitle = (TextView) findViewById(R.id.compli_content_subtitle);
+        care = (TextView) findViewById(R.id.compli_content_care);
+        careTitle = (TextView) findViewById(R.id.compli_content_title_care);
+        img = (ImageView) findViewById(R.id.compli_content_img);
+
+        backBtn.setOnClickListener(this);
+
+        //filter intent
+        Intent intent = getIntent();
+        content = (int) intent.getIntExtra("content", 0);
+        if (content == 1) {
+            img.setImageResource(R.drawable.img_4_1_1);
+            title.setText(getString(R.string.compli_content_one_title));
+            subtitle.setText(getString(R.string.compli_content_one_subtitle));
+            care.setText(getString(R.string.compli_content_one_care));
+        }
+        else if (content == 2) {
+            img.getLayoutParams().height = 0;
+            img.getLayoutParams().width = 0;
+            img.requestLayout();
+            title.setText(getString(R.string.compli_content_two_title));
+            care.setText(getString(R.string.compli_content_one_care));
+        }
+        else if (content == 3) {
+            img.getLayoutParams().height = 0;
+            img.getLayoutParams().width = 0;
+            img.requestLayout();
+            title.setText(getString(R.string.compli_content_three_title));
+            care.setText(getString(R.string.compli_content_three_care));
+        }
+        else if (content == 4) {
+            img.setImageResource(R.drawable.img_4_1_4);
+            title.setText(getString(R.string.compli_content_four_title));
+            subtitle.setText(getString(R.string.compli_content_four_subtitle));
+            care.setText(getString(R.string.compli_content_four_care));
+        }
+        else if (content == 5) {
+            img.getLayoutParams().height = 0;
+            img.getLayoutParams().width = 0;
+            img.requestLayout();
+            title.setText(getString(R.string.compli_content_five_title));
+            subtitle.setText(getString(R.string.compli_content_five_subtitle));
+            care.setText(getString(R.string.compli_content_five_care));
+        }
+        else if (content == 6) {
+            img.setImageResource(R.drawable.img_4_1_6);
+            title.setText(getString(R.string.compli_content_six_title));
+            subtitle.setText(getString(R.string.compli_content_six_subtitle));
+            care.setText(getString(R.string.compli_content_six_care));
+        }
+        else if (content == 7) {
+            img.setImageResource(R.drawable.img_4_1_7);
+            title.setText(getString(R.string.compli_content_seven_title));
+            subtitle.setText(getString(R.string.compli_content_seven_subtitle));
+            care.setText(getString(R.string.compli_content_seven_care));
+            careTitle.setText("");
+            careTitle.setBackgroundResource(R.color.white);
+        }
+        else if (content == 8) {
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == backBtn) {
+            finish();
+        }
+    }
+}
