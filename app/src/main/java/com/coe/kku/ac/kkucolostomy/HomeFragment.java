21b192364c9f7df18b107cd9abc66a201068be4a
chompusama <chadaporn.p@kkumai.com>
2019-10-19 23:32:16 +0700

    update home fragment

--- app/src/main/java/com/coe/kku/ac/kkucolostomy/HomeFragment.java
+++ app/src/main/java/com/coe/kku/ac/kkucolostomy/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.coe.kku.ac.kkucolostomy;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,13 +10,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.IOException;
 
 
 public class HomeFragment extends Fragment {
+
+    private Button complicatoinBtn, communicationBtn, routineBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        routineBtn = (Button) view.findViewById(R.id.home_routine_btn);
+        communicationBtn = (Button) view.findViewById(R.id.home_commu_btn);
+        complicatoinBtn = (Button) view.findViewById(R.id.home_compli_btn);
+
+
+        routineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        communicationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        complicatoinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ComplicationActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
 
         return view;
     }
