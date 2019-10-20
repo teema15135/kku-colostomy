c5a025c4b8d5137cbec7d3592451221a9d211adc
chompusama <chadaporn.p@kkumai.com>
2019-10-20 16:55:08 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ app/src/main/java/com/coe/kku/ac/kkucolostomy/CommunicationActivity.java
@@ -0,0 +1,14 @@
+package com.coe.kku.ac.kkucolostomy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommunicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_communication);
+    }
+}
