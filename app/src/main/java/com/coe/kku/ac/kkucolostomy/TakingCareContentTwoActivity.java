396b4c8c6477827aecf62e511346743c07fe2dba
chompusama <chadaporn.p@kkumai.com>
2019-10-20 17:26:59 +0700

    add all taking care content activities

---
+++ app/src/main/java/com/coe/kku/ac/kkucolostomy/TakingCareContentTwoActivity.java
@@ -0,0 +1,14 @@
+package com.coe.kku.ac.kkucolostomy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TakingCareContentTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taking_care_content_two);
+    }
+}
