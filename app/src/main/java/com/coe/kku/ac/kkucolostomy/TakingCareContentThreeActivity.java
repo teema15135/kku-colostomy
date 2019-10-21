fee4dadce9b91446aedb7fe932c1e33259074541
chompusama <chadaporn.p@kkumai.com>
2019-10-22 02:26:16 +0700

    add taking care 2.3 activity

--- app/src/main/java/com/coe/kku/ac/kkucolostomy/TakingCareContentThreeActivity.java
+++ app/src/main/java/com/coe/kku/ac/kkucolostomy/TakingCareContentThreeActivity.java
@@ -2,13 +2,26 @@ package com.coe.kku.ac.kkucolostomy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TakingCareContentThreeActivity extends AppCompatActivity {
+
+    private ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taking_care_content_three);
+
+        backBtn = (ImageButton) findViewById(R.id.tc_three_back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
